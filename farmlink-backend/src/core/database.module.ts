49f5bf0471bc3feb6b9b20f2666fbba3e22ba982
Chimix <lavireak9@gmail.com>
2026-05-17 22:11:32 +0700

    host database to supabase

--- farmlink-backend/src/core/database.module.ts
+++ farmlink-backend/src/core/database.module.ts
@@ -59,11 +59,13 @@ export const ALL_ENTITIES = [
       
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
         host: config.get<string>('DB_HOST', 'localhost'),
         port: config.get<number>('DB_PORT', 5432),
         username: config.get<string>('DB_USERNAME') || config.get<string>('DB_USER'),
         password: String(config.get('DB_PASSWORD') || config.get('DB_PASS') || ''),
         database: config.get<string>('DB_NAME'),
+        ssl: config.get<string>('DB_HOST') !== 'localhost' && config.get<string>('DB_HOST') !== 'postgres' ? { rejectUnauthorized: false } : false,
         entities: ALL_ENTITIES,
         synchronize: config.get<string>('NODE_ENV') !== 'production',
         logging: false,
