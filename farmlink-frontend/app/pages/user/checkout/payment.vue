ab37984e189ff126d20730c660e9795389326723
MI-Jerng <chimoengiphone@gmail.com>
2026-05-25 13:20:07 +0700

    debugging code and fix missing part

--- farmlink-frontend/app/pages/user/checkout/payment.vue
+++ farmlink-frontend/app/pages/user/checkout/payment.vue
@@ -263,7 +263,7 @@ async function processPayment() {
   } else {
     // Send the paymentMethod.id to our NestJS backend to actually charge the card!
     try {
-      const response = await fetch('http://localhost:3001/stripe/charge', {
+      const response = await fetch('http://localhost:3001/api/stripe/charge', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
